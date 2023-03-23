07c906e6588d1971097f6b24b5874619395ae5dc
egin10 <egin066@gmail.com>
2023-03-23 11:34:05 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ginanjar Setia Budi on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
